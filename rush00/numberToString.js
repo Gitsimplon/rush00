400be33f90d947a7cfbf629c8ce8ce76f26a61ac
Git- Rambodevv <103598800+Gitsimplon@users.noreply.github.com>
2023-02-15 09:57:45 +0100

    Update numberToString.js

--- rush00/numberToString.js
+++ rush00/numberToString.js
@@ -1,17 +1,22 @@
-var input =[1,2,3,4,5,6,7,8,9,10];
+var input =[1,2,3,5,10];
 function numberToString(_numbers){
 var index = 0;
-var numbers = {un:1,deux:2,trois:3,quatre:4,cinq:5,six:6,sept:7,huit:8,neuf:9,dix:10};
+var dictionnaire = {un:1,deux:2,trois:3,quatre:4,cinq:5,six:6,sept:7,huit:8,neuf:9,dix:10};
+this.letteres = new Array();
 this.entiers = new Array();
-for ( [ key,value] of Object.entries(numbers)) {
-
-    _numbers[index] = key
+for ( [ key,value] of Object.entries(dictionnaire)) {
+     for( index = 0; index < _numbers.length ; index++)
+     {
+if(_numbers[index] === value)
+   { 
+     letteres[index] = key
     entiers[index] = value;
-    index++
-    if(index == _numbers.length)
-    break;
-}
-console.log(_numbers) ;
+   }
+    
+     }
+
+   }
+console.log(letteres);
 }
 numberToString(input);
 module.exports = numberToString ;
